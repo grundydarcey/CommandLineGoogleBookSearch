01b47147eb7be0a77e605020cd2f7e65156568cb
Beyonce Knowles <you@example.com>
2021-06-14 13:59:46 -0400

    search result tweak

--- books.js
+++ books.js
@@ -2,10 +2,19 @@
 import config from './config.js';
 import fetch from 'node-fetch';
 
+let bookList = [];
+
 const play = function() {
   console.log('Type a query to search from some Google books.');
   process.openStdin().on('data', function(chunk) {
-    searchResult(chunk.toString('utf8'));
+    // fetch(`${config.API_ENDPOINT}?q=${chunk}`)
+    //   .then(response => response.json())
+    //   .then(data => bookList.push(data.items.slice(0, 5)));
+    if (chunk.toString('utf8').length >= 4) {
+      searchResult(chunk);
+    } else {
+      displayReadingList(chunk);
+    }
   });
 };
 
@@ -15,19 +24,26 @@ const searchResult = function(chunk) {
     .then(response => response.json())
     .then(data => (data.items).slice(0, 5).forEach((books, i) => {
       console.log(`${i + 1}` + '. ' + books['volumeInfo']['title'] + ' by ' + books['volumeInfo']['authors'] + ', published by ' + books['volumeInfo']['publisher']);
-    }));
+    }))
+    .then(items => console.log(items.slice(0, 5)[0]));
+  //.then(data => bookList.push(data.items.slice(0, 5)));
+  console.log(bookList);
+  //   console.log(bookList)
   addReadingList();
 };
 
 const addReadingList = function() {
   console.log('If you would like to add any of these books to your reading list, enter its corresponding number below.');
-  process.openStdin().on('data', function(chunk) {
-    displayReadingList(chunk.toString('utf8'));
-  });
 };
 
 const displayReadingList = function(chunk) {
-  console.log('You chose book number ' + chunk);
+  console.log('Good choice! You chose to add book number ' + chunk);
+  let bookIndex = chunk - 1;
+  let books = bookList[0];
+  let readingList = [];
+  readingList.push(books[bookIndex]['volumeInfo']['title'] + ' by ' + books[bookIndex]['volumeInfo']['authors'] + ', published by ' + books[bookIndex]['volumeInfo']['publisher']);
+  console.log(readingList);
+  bookList = [];
 };
 
 play();
