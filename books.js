28d8a9d0674e5265654798095ca54f05fd6780cb
Beyonce Knowles <you@example.com>
2021-06-14 14:29:02 -0400

    error handling

--- books.js
+++ books.js
@@ -3,17 +3,22 @@ import config from './config.js';
 import fetch from 'node-fetch';
 
 let bookList = [];
+let readingList = [];
 
 const play = function() {
-  console.log('Type a query to search from some Google books.');
+  console.log('Type a query to search from Google books.');
   process.openStdin().on('data', function(chunk) {
-    // fetch(`${config.API_ENDPOINT}?q=${chunk}`)
-    //   .then(response => response.json())
-    //   .then(data => bookList.push(data.items.slice(0, 5)));
     if (chunk.toString('utf8').length >= 4) {
+      fetch(`${config.API_ENDPOINT}?q=${chunk}`)
+        .then(response => response.json())
+        .then(data => bookList.push(data.items.slice(0, 5)));
       searchResult(chunk);
     } else {
-      displayReadingList(chunk);
+      if (chunk.toString('utf8') >= 6 || chunk.toString('utf8') <= 0) {
+        console.log('Enter a number between 1-5.');
+      } else {
+        displayReadingList(chunk);
+      }
     }
   });
 };
@@ -25,10 +30,9 @@ const searchResult = function(chunk) {
     .then(data => (data.items).slice(0, 5).forEach((books, i) => {
       console.log(`${i + 1}` + '. ' + books['volumeInfo']['title'] + ' by ' + books['volumeInfo']['authors'] + ', published by ' + books['volumeInfo']['publisher']);
     }))
-    .then(items => console.log(items.slice(0, 5)[0]));
-  //.then(data => bookList.push(data.items.slice(0, 5)));
-  console.log(bookList);
-  //   console.log(bookList)
+    .catch(err => {
+      console.error(err);
+    });
   addReadingList();
 };
 
@@ -37,13 +41,13 @@ const addReadingList = function() {
 };
 
 const displayReadingList = function(chunk) {
-  console.log('Good choice! You chose to add book number ' + chunk);
+  console.log('Good choice! You chose to add book number ' + chunk + 'Your reading list so far:');
   let bookIndex = chunk - 1;
   let books = bookList[0];
-  let readingList = [];
   readingList.push(books[bookIndex]['volumeInfo']['title'] + ' by ' + books[bookIndex]['volumeInfo']['authors'] + ', published by ' + books[bookIndex]['volumeInfo']['publisher']);
   console.log(readingList);
   bookList = [];
+  console.log('Type another query to search from Google books.');
 };
 
 play();
